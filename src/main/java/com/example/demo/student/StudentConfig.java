c912a89fe1f9c3d59042dae8695ef6f5210e6e43
Gustavo Rozato Rodrigues <rozato.gustavo@gmail.com>
2023-02-10 04:31:17 -0300

    spring CRU student

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mari = new Student(
+                    "Mari",
+                    "email@email.com",
+                    LocalDate.of(2000, Month.JANUARY, 5)
+            );
+
+            Student alex = new Student(
+                    "Alex",
+                    "email2@email.com",
+                    LocalDate.of(2003, Month.JANUARY, 5)
+            );
+
+            repository.saveAll(List.of(mari, alex));
+        };
+    }
+}
---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,39 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping(produces = "application/json")
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long id) {
+        studentService.deleteStudent(id);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long id,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        studentService.updateStudent(id, name, email);
+    }
+}
